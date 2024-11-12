56e0959e3b8c0f8135fe470e747aee881b6820d1
Foxinat <vanlak21808@gmail.com>
2024-11-12 22:15:54 +0300

    практикум7

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)   
+        {
+            //Task1 
+            //Console.Write("введите ваше имя");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Ваше имя - {name}.");
+            //Console.WriteLine($"Здравствуй {name}.");
+            //Console.Read();
+            //Task2
+            //Console.Write("Введите название футбольной команды ");
+            //string team = Console.ReadLine();
+            //Console.WriteLine($"{team} - Чемпион!.");
+            //Console.Read();
+
+            //Task 3
+            //Console.Write("Введите ваше имя ");
+            //string name = Console.ReadLine();
+            //Console.Write("Введите вашу Фамилию ");
+            //string surname = Console.ReadLine();
+            //Console.WriteLine($"Вы-{name}\t{surname}");
+            //Console.Read();
+
+            //task 4
+            //Console.Write("Введите государство ");
+            //string state = Console.ReadLine();
+            //Console.Write("Введите Столицу ");
+            //string capital = Console.ReadLine();
+            //Console.WriteLine($"Столица государства {state} - город {capital}");
+            //Console.Read();
+
+            //task 5
+            //Console.Write("Введите название романа ");
+            //string roman = Console.ReadLine();
+            //Console.Write("Введите его автора ");
+            //string author = Console.ReadLine();
+            //Console.WriteLine($"Писатель {author} — автор романа {roman}");
+            //Console.Read();
+
+            //task 6
+            //Console.Write("Введите название футбольного клуба ");
+            //string club = Console.ReadLine();
+            //Console.WriteLine($"Количество символов:{club.Length}");
+            //Console.Read();
+            
+            //Task7
+            //Console.WriteLine("Ты вчера был болен.");
+            //Console.WriteLine("Измерь - ка температуру!");
+            //Console.Write("Сообщи, какая у тебя температура: ");
+            //double temperature = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Ты здоров, дружок!");
+            //Console.WriteLine("Можешь идти в школу.");
+            //Console.WriteLine("Желаю успехов!");
+            //Console.Read();
+
+            //task 8
+            //Console.Write("Здравствуй, я компьютер, а тебя как зовут? ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Очень приятно, {name}.");
+            //Console.Write("Сколько тебе лет? ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Ого! Целых {age} лет! Ты уже совсем взрослый!");
+            //Console.Read();
+
+            //Task 9
+            //Console.Write("Как тебя зовут? ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Привет, {name}.");
+            //Console.Write("Сколько тебе лет? ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.Write("Какой у тебя любимый предмет в техникуме? ");
+            //string subject = Console.ReadLine();
+            //Console.Write("Какая у тебя любимая книга? ");
+            //string book = Console.ReadLine();
+            //Console.WriteLine($"Значит тебя зовут, {name}");
+            //Console.WriteLine($"Тебе {age} лет");
+            //Console.WriteLine($"Твой любимый предмет - {subject}");
+            //Console.WriteLine($"Твоя любимая книга - {book}");
+            //Console.Read();
+
+            //task 10
+            //Console.Write("Как тебя зовут? ");
+            //string name = Console.ReadLine();
+            //Console.Write("Сколько тебе лет? ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{name}, ты родился в {2024 - age} году.");
+            //Console.Read();
+
+        }
+    }
+}
